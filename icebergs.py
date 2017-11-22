edb9f0a718b45e130f133609029c3bc8e310aca0
Daniel <dsq@cin.ufpe.br>
2017-11-22 01:19:06 -0300

    tratando test e outras mudanças

--- icebergs.py
+++ icebergs.py
@@ -2,36 +2,60 @@ import pandas as pd
 import numpy as np
 from sklearn.svm import SVR
 
+def main():
+	train_df = pd.read_json('train.json')
+	print('passou treino')
 
-train_df = pd.read_json('train.json')
-print('passou treino')
-test_df = pd.read_json('test.json')
-print('passou teste')
+	x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75)
+						for band in train_df["band_1"]])
+	x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75)
+						for band in train_df["band_2"]])
+	X_train = np.concatenate([x_band1[:, :, :, np.newaxis],
+							  x_band2[:, :, :, np.newaxis]], axis=-1)
+	Y_train = np.array(train_df["is_iceberg"])
+	vars = X_train.shape
+	d2_train = X_train.reshape(vars[0], vars[1] * vars[2] * vars[3])
 
-x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_1"]])
-x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_2"]])
-X_train = np.concatenate([x_band1[:, :, :, np.newaxis], x_band2[:, :, :, np.newaxis]], axis=-1)
-Y_train = np.array(train_df["is_iceberg"])
-vars = X_train.shape
-d2_train = X_train.reshape(vars[0], vars[1] * vars[2] * vars[3])
+	print("Xtrain:", X_train.shape)
+	print("Ytrain:", Y_train.shape)
+	print(Y_train)
 
-print("Xtrain:", X_train.shape)
-print("Ytrain:", Y_train.shape)
-print(Y_train)
 
-x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_1"]])
-x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_2"]])
-X_test = np.concatenate([x_band1[:, :, :, np.newaxis], x_band2[:, :, :, np.newaxis]], axis=-1)
-print("Xtest:", X_test.shape)
+	clf = SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.2,
+			  gamma='auto', kernel='rbf', max_iter=-1, shrinking=True,
+			  tol=0.001, verbose=False)
 
-clf = SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='auto', kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+	clf.fit(d2_train, Y_train)
+	
+	print('treinou')
 
-clf.fit(d2_train, Y_train)
+	#
+	response = huge_predict('test2.json', clf, chunksize=337)
 
-response = clf.predict(X_test)
+	print()
 
-print('treinou')
+	print(response)
+	print(response.shape)
 
-print()
-
-print(response)
+def huge_predict(filepath, clf, chunksize=4):
+    reader = pd.read_json(filepath, lines=True, chunksize=chunksize)
+    prediction = np.array([])
+    #i = 0
+    for chunk in reader:
+        x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75)
+                            for band in chunk["band_1"]])
+        x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75)
+                            for band in chunk["band_2"]])
+        X_test = np.concatenate([x_band1[:, :, :, np.newaxis],
+                                 x_band2[:, :, :, np.newaxis]], axis=-1)
+        test_vars = X_test.shape
+        d2_test = X_test.reshape(test_vars[0], test_vars[1] *
+                                 test_vars[2] * test_vars[3])
+        response = clf.predict(d2_test)
+        prediction = np.concatenate((prediction, response))
+        #i = i + 1
+        #if i > 1:
+        #    break
+    return prediction
+	
+main()
